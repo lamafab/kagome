d5710c437e108b2d810306cb84a8f4172e38b9b3
Harrm <shcherba@soramitsu.co.jp>
2020-01-15 21:47:34 +0300

    Work on interfaces for changes trie

--- core/blockchain/changes_trie.hpp
+++ core/blockchain/changes_trie.hpp
@@ -3,7 +3,27 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef KAGOME_CHANGES_TRIE_HPP
-#define KAGOME_CHANGES_TRIE_HPP
+#ifndef KAGOME_BLOCKCHAIN_CHANGES_TRIE_HPP
+#define KAGOME_BLOCKCHAIN_CHANGES_TRIE_HPP
 
-#endif //KAGOME_CHANGES_TRIE_HPP
+#include "outcome/outcome.hpp"
+#include "common/blob.hpp"
+
+namespace kagome::blockchain {
+
+  class OverlayedChanges;
+
+  class ChangesTrie {
+   public:
+    static outcome::result<ChangesTrie> create(
+        const OverlayedChanges &changes,
+        backend,
+        storage,
+        const common::Hash256 &parent_hash);
+
+   private:
+  };
+
+}
+
+#endif  // KAGOME_BLOCKCHAIN_CHANGES_TRIE_HPP
